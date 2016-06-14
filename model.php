147a278a6a9c45fffe59a2da99921cf347326c1a
Mario Mauricette <mmauri04@hotmail.fr>
2016-06-14 10:44:14 +0200

    Create model.php
    Add a the file model.php

---
+++ model.php
@@ -0,0 +1,33 @@
+<?php 
+
+
+function open_database_connection()
+{
+      $bdd = new PDO('mysql:host=localhost;dbname=mesnews;charset=utf8', 'mesnews_user', 'secret');
+
+      $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      return $bdd;
+      $news = $bdd->query('select * from T_NEWS order by CAT_ID desc');
+}
+
+function close_database($bdd)
+{
+      $bdd = null;
+}
+
+function getNews()
+{
+      $bdd = open_database_connection();
+
+      $sql = "SELECT * FROM T_NEWS ORDER BY CAT_ID DESC";
+      $result = $bdd->query($sql);
+
+      $news = array();
+
+      while($row = $result->fetch(PDO::FETCH_ASSOC)){
+            $news[] = $row;
+      }
+      close_database($bdd);
+      return $news;
+}
